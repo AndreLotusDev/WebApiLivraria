ba20673a3c73d626f31883a2851f881ade0679f0
Andre Soares Gomes <andrsoares953@yahoo.com>
2021-02-18 00:50:32 -0300

    Maldito erro q atrapalhou meu sistema

--- WebApiLivraria/Startup.cs
+++ WebApiLivraria/Startup.cs
@@ -59,6 +59,8 @@ namespace WebApiLivraria
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -71,7 +73,6 @@ namespace WebApiLivraria
             app.UseRouting();
 
             app.UseAuthorization();
-            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
